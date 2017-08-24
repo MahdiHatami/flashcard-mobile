ccd1c4c034b4b9f514db684f5bb4053515f6dc07
Mahdi Hatami <iz.hatami@gmail.com>
2017-08-24 16:15:44 +0300

    new landing page - work in progress

---
+++ app/src/main/java/com/mutlak/metis/wordmem/features/landing/LanActivity.kt
@@ -0,0 +1,37 @@
+package com.mutlak.metis.wordmem.features.landing
+
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Interpolator
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.mutlak.metis.wordmem.R
+import com.mutlak.metis.wordmem.features.result.widget.CircleProgressView
+
+
+class LanActivity : AppCompatActivity() {
+
+  @BindView(R.id.circle_progress_view) lateinit var mCircleProgressView: CircleProgressView
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_lan)
+    ButterKnife.bind(this)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      val window = window
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+      window.statusBarColor = ContextCompat.getColor(this, R.color.landing_header_center)
+    }
+
+    mCircleProgressView.isTextEnabled = true
+    mCircleProgressView.interpolator = AccelerateDecelerateInterpolator() as Interpolator?
+    mCircleProgressView.setProgressWithAnimation(60F, 1000)
+  }
+
+
+}
--- app/src/main/java/com/mutlak/metis/wordmem/features/main/MainActivity.kt
+++ app/src/main/java/com/mutlak/metis/wordmem/features/main/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mutlak.metis.wordmem.data.local.PreferencesHelper
 import com.mutlak.metis.wordmem.features.intro.IntroActivity
-import com.mutlak.metis.wordmem.features.landing.LandingActivity
+import com.mutlak.metis.wordmem.features.landing.LanActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     val isShowed = mPref.getBoolean(IntroActivity.SHOW_APP_INTRO, false)
     if (isShowed) {
-      startActivity(Intent(this, LandingActivity::class.java))
+      startActivity(Intent(this, LanActivity::class.java))
     } else {
       startActivity(Intent(this, IntroActivity::class.java))
     }
